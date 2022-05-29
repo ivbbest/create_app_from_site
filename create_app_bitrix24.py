b54ead13105702a76a95238041c6bc2becc6bc36
Vladimir Irkhin <ivb1989@yandex.ru>
2022-05-29 12:22:57 +0300

    Удаление всего лишнего. Корректировка PEP8.

--- create_app_bitrix24.py
+++ create_app_bitrix24.py
@@ -1,8 +1,5 @@
 from fast_bitrix24 import Bitrix
-from datetime import datetime, timedelta
-import requests
 from config import WEBHOOK  # конфигурационный файл с вебхуком
-from pprint import pprint
 from options import userfields, app_from_site, filter_delivery_date, \
     filter_delivery_adress, filter_delivery_code
 
@@ -11,13 +8,13 @@ webhook = WEBHOOK
 b = Bitrix(webhook)
 
 
-def add_userfield(userfields):
+def add_userfield(custom_tags):
     """
     Создание пользовательских тегов по списку.
     Если поля такие есть, то пропускается работа с перехватом ошибки.
     """
     try:
-        for userfield in userfields:
+        for userfield in custom_tags:
             print(userfield)
 
             new_userfield = [
@@ -213,164 +210,6 @@ def main(purchase):
     except Exception as err:
         print('Any Error', err)
 
-    # for i in range(273, 295, 2):
-    #     try:
-    #         print(b.call('crm.deal.userfield.delete', {'ID': i}))
-    #     except Exception as e:
-    #         print(e)
-    # add_userfield(userfields)
-    # # # analyze_delivery_code(app_from_site)
-    # # pprint(b.call('crm.deal.userfield.get', {'ID': 291})['FIELD_NAME'])
-    # deals = b.call('crm.deal.list', {
-    #     'select': ['UF_*'],
-    #     'filter': {'CLOSED': 'N'}
-    # })[0]
-    # # pprint(deals)
 
-
-# if __name__ == '__main__':
-#     main(app_from_site)
-pprint(b.get_all('crm.deal.userfield.list'))
-###############################################################
-###############################################################
-
-# app = {
-#     "title": "title",
-#     "description": "Some description",
-#     "client": {
-#         "name": "Jon",
-#         "surname": "Karter",
-#         "phone": "+77777777777",
-#         "adress": "st. Mira, 287, Moscow"
-#     },
-#     "products": ["Candy", "Carrot", "Potato"],
-#     "delivery_adress": "st. Mira, 211, Ekaterinburg",
-#     "delivery_date": "2021-01-01:16:00",
-#     "delivery_code": "#232nkF3fAdn"
-# }
-#
-# app2 = [
-#     {
-#         'ID': 1,
-#         'fields': {
-#             'TITLE': '3 New tasks in Bitrix',
-#             'RESPONSIBLE_ID': 1,
-#             'CREATED_BY': 1
-#         }
-#     }
-# ]
-#
-# sdelka = [
-#     {
-#         'fields': {
-#             "TITLE": "title28",
-#             "SOURCE_DESCRIPTION": "Some description28",
-#             'CONTACT_ID': 3,
-#             'UF_CRM_MY_STRING': "#652nkF6fAfdn"
-#
-#         }
-#     }
-# ]
-# #
-# result = b.call('crm.deal.add', sdelka)
-# breakpoint()
-#
-# # print(b.call('crm.deal.contact.fields', {}))
-#
-# params = {"products": ["Candy", "Carrot", "Potato"],
-#           "delivery_adress": "st. Mira, 211, Ekaterinburg",
-#           "delivery_date": "2021-01-01:16:00",
-#           "delivery_code": "#232nkF3fAdn"
-#           }
-#
-# # print(b.call('crm.contact.get', {'id': 3}))
-#
-# # print(b.call('crm.deal.get', {'id': 5}))
-#
-# # print(b.call('crm.productrow.fields', {})
-#
-#
-# userfield = [
-#     {
-#         'fields': {
-#             "FIELD_NAME": "MY_STRING3",
-#             "EDIT_FORM_LABEL": "Моя строка777",
-#             "LIST_COLUMN_LABEL": "Моя строка3",
-#             "USER_TYPE_ID": "string"
-#
-#         }
-#     }
-# ]
-#
-#
-# # print(b.call('crm.deal.userfield.add', userfield))
-#
-#
-#
-# userfilter = [
-#     {
-#         # 'order': {"ID": "ASC"},
-#         'filter': {'UF_CRM_MY_STRING3': "64647hjhff"},
-#         'select': ['*']
-#     },
-# ]
-#
-#
-# # print(b.call('crm.deal.list', userfilter))
-# print(b.call('crm.productrow.fields', {}))
-
-# products = app_from_site['products']
-# rows = [{"PRODUCT_NAME": product} for product in products]
-
-# for product in products:
-#     rows.append({"PRODUCT_NAME": product})
-
-
-# product_name = [
-#     {
-#         'ID': 21,
-#         'rows': rows,
-#     }
-#
-# ]
-# print(b.call('crm.deal.productrows.set', product_name))
-
-#
-#
-# product_name = [
-#     {
-#         'ID': 9,
-#         'rows': [
-#             ["Candy", "Carrot", "Potato"]
-#         ]
-#     }
-# ]
-#
-# # print(b.call('crm.userfield.fields', {}))
-#
-#
-# new_user = {
-#         'fields': {
-#             "NAME": "Masha",
-#             "SECOND_NAME": "Masha",
-#             "LAST_NAME": "Masha",
-#             "PHONE": [{"VALUE": "+4444"}]
-#         }
-#     }
-#
-# result = b.call('crm.contact.add', new_user)
-# breakpoint()
-
-
-
-#
-#
-# user_phone = [
-#     {
-#         'filter': {'PHONE': "+4444"},
-#         'select': ['ID']
-#     },
-# ]
-#
-# user_id = b.call('crm.contact.list', user_phone)
-# breakpoint()
+if __name__ == '__main__':
+    main(app_from_site)
